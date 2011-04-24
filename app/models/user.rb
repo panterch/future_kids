e2b0613495a2fa7633eb47e8c16c5b4a5f54a6ed
seb <seb@panter.ch>
2011-04-24 20:12:00 +0200

    conditional password change implemented

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,15 @@ class User
          :recoverable, :rememberable, :trackable, :validatable
 
   # attr_accessible :email, :password, :password_confirmation, :remember_me
+  
+  before_validation :nilify_blank_password
+
+protected
+
+  def nilify_blank_password
+    if password.blank? && password_confirmation.blank?
+      self.password = self.password_confirmation = nil
+    end
+  end
 
 end
--- spec/models/mentor_spec.rb
+++ spec/models/mentor_spec.rb
@@ -14,10 +14,16 @@ describe Mentor do
       mentor.password = mentor.password_confirmation = nil
       mentor.should be_valid
     end
-    # this is what happens when sending empty password field with a form
+    # devise triggers an error when empty passwords are entered. but this
+    # is the default when submitting a form
     it 'does trigger an error on blank password' do
       mentor.password = mentor.password_confirmation = ''
-      mentor.should_not be_valid
+      mentor.should be_valid
+    end
+    it 'does not allow nil password on unsaved mentor' do
+      new_mentor = Factory.build(:mentor)
+      new_mentor.password = new_mentor.password_confirmation = nil
+      new_mentor.should_not be_valid
     end
   end
 end
