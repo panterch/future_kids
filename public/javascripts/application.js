37dea0c88dddd877e2ea5a15d12586181d5a62b9
seb <seb@panter.ch>
2011-06-11 10:58:01 +0200

    expandable sub menu

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,16 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(function () {
+  register_subnav_expand();
+});
+
+// navigation items with further nesting are handled specially:
+// they are not displayed, but clicking on the elements above the sub-sub list
+function register_subnav_expand() {
+  $('ul > li > ul').parents('li').hide();
+  $('ul > li > ul').each(function() {
+    $(this).parent().prev().click(function() {
+		  $(this).next().toggle('fast');
+		  return false;
+    });
+  });
+}
+
