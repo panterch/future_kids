f05eb246f829eee0df06b1fe1c48a6e004b2d947
seb <seb@panter.ch>
2011-10-03 12:14:58 +0200

    added host for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,6 +36,7 @@ FutureKids::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => 'kids.panter.ch' }  
 
   # Enable threaded mode
   # config.threadsafe!
