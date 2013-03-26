596a99b70e435dc93170bc7dd901e56b6f353670
seb <seb@panter.ch>
2013-03-26 16:24:48 +0100

    changed email address

--- app/mailers/notifications.rb
+++ app/mailers/notifications.rb
@@ -1,5 +1,5 @@
 class Notifications < ActionMailer::Base
-  default :from => "AOZ Future Kids <futurekids@panter.ch>"
+  default :from => "AOZ Future Kids <info@aoz-futurekids.ch>"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
@@ -8,12 +8,12 @@ class Notifications < ActionMailer::Base
   #
   def remind(reminder)
     @reminder = reminder
-    mail :to => @reminder.recipient, :bcc => "futurekids@panter.ch"
+    mail :to => @reminder.recipient, :bcc => "info@aoz-futurekids.ch"
   end
 
   def reminders_created(count)
     @count = count
-    mail :to => "futurekids@panter.ch"
+    mail :to => "info@aoz-futurekids.ch"
   end
 
   def test(to)
