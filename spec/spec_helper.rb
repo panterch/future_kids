f898e5c58e2c0acb4c8288ab0957c89e156ff6ce
seb <seb@panter.ch>
2014-05-29 18:24:49 +0300

    removed spork

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,34 +1,19 @@
 require 'rubygems'
-require 'spork'
 
 ENV["RAILS_ENV"] ||= 'test'
 
-Spork.prefork do
+require "rails/application"
 
-  # https://github.com/timcharper/spork/wiki/Spork.trap_method-Jujutsu
-  require "rails/application"
-  Spork.trap_method(Rails::Application, :reload_routes!)
+# Loading more in this block will cause your tests to run faster. However,
+# if you change any configuration or code from libraries loaded here, you'll
+# need to restart spork for it take effect.
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'views/inherited_resource_helpers'
 
-  # Loading more in this block will cause your tests to run faster. However,
-  # if you change any configuration or code from libraries loaded here, you'll
-  # need to restart spork for it take effect.
-  require File.expand_path("../../config/environment", __FILE__)
-  require 'rspec/rails'
-  require 'views/inherited_resource_helpers'
-
-  # Requires supporting ruby files with custom matchers and macros, etc,
-  # in spec/support/ and its subdirectories.
-  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
-end
-
-Spork.each_run do
-  Rails.application.reload_routes!
-end
-
-Spork.each_run do
-  FactoryGirl.factories.clear
-  load 'spec/factories.rb'
-end
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 
 RSpec.configure do |config|
