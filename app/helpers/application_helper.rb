acb30be8da9c5e7faa9000e4f02d28f187071664
Andi Staub <sta@panter.ch>
2014-05-30 13:19:57 +0300

    Add nav_link helper for link highlighting.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -121,5 +121,11 @@ module ApplicationHelper
     tags
   end
 
+  def nav_link(link_text, link_path)
+    class_name = current_page?(link_path) ? 'active' : ''
 
+    content_tag(:li, :class => class_name) do
+      link_to link_text, link_path
+    end
+  end
 end
