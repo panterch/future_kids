0b3f97ca36af26c9ce205d5c049c81711be73558
Beat Seeliger <seb@panter.ch>
2014-12-26 21:57:29 +0100

    filter inactive principals

--- app/controllers/principals_controller.rb
+++ app/controllers/principals_controller.rb
@@ -1,4 +1,20 @@
-class PrincipalsController < InheritedResources::Base
+class PrincipalsController < ApplicationController
+
+  inherit_resources
   load_and_authorize_resource
 
+  before_filter :accessible_by_error_quick_fix
+
+  def index
+    # a prototyped principal is submitted with each index query. if the
+    # prototype is not present, it is built here with default values
+    params[:principal] ||= {}
+    params[:principal][:inactive] = "0" if params[:principal][:inactive].nil?
+    @principals = @principals.where(params[:principal].to_h.delete_if {|key, val| val.blank? })
+
+    # provide a prototype principal for the filter form
+    @principal = Principal.new(permitted_params[:principal])
+
+    index!
+  end
 end
