301453b4b4b82fe11073dcbbae793c7be3e8eabc
m1nuted <raffael.f.pvt@gmail.com>
2015-05-05 14:39:30 +0200

    admin_update_school_spec implemented
    It checks if an admin can modify an existing school

--- spec/requests/admin_updating_school_spec.rb
+++ spec/requests/admin_updating_school_spec.rb
@@ -8,16 +8,19 @@ feature 'ADMIN::UPDATE:SCHOOL', '
       @pw = 'welcome'
       @admin = create(:admin, password: @pw, password_confirmation: @pw)
       log_in(@admin)
+      @school = create(:school, name:'SSIG')
   end
 
   scenario 'should be able to modify an existing school' do
       click_link 'Schule'
+      click_link 'SSIG'
       click_link 'Bearbeiten'
-      click_link school.name
+
       fill_in 'Name', with: 'School name'
       fill_in 'Strasse, Nr.', with: 'street'
       click_button 'Schule aktualisieren'
+
       expect(page.status_code).to eq(200)
-      expect(page).to have_content('Schule anzeigen')
+      
   end
 end
