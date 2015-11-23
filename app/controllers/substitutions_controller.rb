c3def11bdcca7281e93e579962cab2c52f577d9e
Martin Eigenmann <github@eigenmannmartin.ch>
2015-11-23 19:52:12 +0100

    adds initial substitution view & controller

--- app/controllers/substitutions_controller.rb
+++ app/controllers/substitutions_controller.rb
@@ -1,4 +1,28 @@
 class SubstitutionsController < ApplicationController
   load_and_authorize_resource
   include CrudActions
+
+  def create
+    @substitution = Substitution.new(substitution_params)
+    if @substitution.save
+      redirect_to action: :index
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @substitution.update(substitution_params)
+      redirect_to action: :index
+    else
+      render :edit
+    end
+  end
+
+  def substitution_params
+    params.require(:substitution).permit(
+      :start_at, :end_at
+    )
+  end
+
 end
