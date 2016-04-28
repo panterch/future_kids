7d3ab5bab9ecfbca02dba2141d8666c3eab758ae
seb <seb@panter.ch>
2016-04-28 14:55:06 +0200

    removed empty spec

--- spec/controllers/substitutions_controller_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe SubstitutionsController do
-	
-  it "shows only relevant substitution"
-  it "marks not resolved substitution red"
-  it "close-substitution link works"
-
-end
