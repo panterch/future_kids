c3b38f447c3bc22a6092ccd24790536acfa0eac6
chrysalag <chrysa.lagodimou@gmail.com>
2016-12-22 15:00:59 +0100

    add test for mentor filtering on coaches and other fields

--- spec/requests/filter_mentors_on_coaches_spec.rb
+++ spec/requests/filter_mentors_on_coaches_spec.rb
@@ -4,12 +4,14 @@ feature 'Mentor index' do
 
   background do
     @pw = 'welcome'
-    @admin  = create(:admin, name: 'first', prename: 'admin', password: @pw, password_confirmation: @pw)
-    @admin2 = create(:admin, name: 'second', prename: 'admin')
-    @mentor = create(:mentor, name: 'first', prename: 'mentor')
-    @kid1   = create(:kid, mentor: @mentor, admin: @admin)
-    @kid2   = create(:kid, mentor: @mentor, admin: @admin2)
-    log_in(@admin)
+    @admin1  = create(:admin, name: 'first', prename: 'admin', password: @pw, password_confirmation: @pw)
+    @admin2  = create(:admin, name: 'second', prename: 'admin')
+    @mentor1 = create(:mentor, name: 'first', prename: 'mentor', transport: 'Halbtax', primary_kids_meeting_day: 'Dienstag')
+    @kid1    = create(:kid, mentor: @mentor1, admin: @admin1)
+    @kid2    = create(:kid, mentor: @mentor1, admin: @admin2)
+    @kid3    = create(:kid, mentor: @mentor1, admin: @admin1)
+    @kid4    = create(:kid, mentor: @mentor1, admin: @admin2)
+    log_in(@admin1)
   end
 
   scenario 'should filter mentors on coaches' do
@@ -23,11 +25,9 @@ feature 'Mentor index' do
   end
 
   scenario 'should filter mentors on coaches without repetitions' do
-    @kid3 = create(:kid, mentor: @mentor, admin: @admin)
-    @kid4 = create(:kid, mentor: @mentor, admin: @admin2)
     @mentor2 = create(:mentor, name: 'second', prename: 'mentor')
-    @kid5 = create(:kid, mentor: @mentor2, admin: @admin2)
-    @kid6 = create(:kid, mentor: @mentor2, admin: @admin2)
+    @kid5    = create(:kid, mentor: @mentor2, admin: @admin2)
+    @kid6    = create(:kid, mentor: @mentor2, admin: @admin2)
     click_link 'Mentor/in'
     select('first admin', from: 'mentor_filter_by_coach_id')
     click_button('Filter anwenden')
@@ -40,4 +40,32 @@ feature 'Mentor index' do
     expect(page).to have_css('a', text: 'second mentor')
   end
 
+  scenario 'filtering on coaches should not affect other filters' do
+    @mentor2 = create(:mentor, name: 'second', prename: 'mentor', transport: 'GA', primary_kids_meeting_day: 'Montag')
+    @kid5    = create(:kid, mentor: @mentor2, admin: @admin2)
+    @kid6    = create(:kid, mentor: @mentor2, admin: @admin2)
+    click_link 'Mentor/in'
+    select('GA', from: 'mentor_transport')
+    click_button('Filter anwenden')
+    expect(page).to have_text ('1 Mentor/innen')
+    select('first admin', from: 'mentor_filter_by_coach_id')
+    click_button('Filter anwenden')
+    expect(page).to have_text ('0 Mentor/innen')
+    select('Halbtax', from: 'mentor_transport')
+    click_button('Filter anwenden')
+    expect(page).to have_text ('1 Mentor/innen')
+    expect(page).to have_css('a', text: 'first mentor')
+    select('GA', from: 'mentor_transport')
+    click_button('Filter anwenden')
+    expect(page).to have_text ('0 Mentor/innen')
+    select('second admin', from: 'mentor_filter_by_coach_id')
+    click_button('Filter anwenden')
+    expect(page).to have_text ('1 Mentor/innen')
+    expect(page).to have_css('a', text: 'second mentor')
+    select('', from: 'mentor_transport')
+    click_button('Filter anwenden')
+    expect(page).to have_text ('2 Mentor/innen')
+    expect(page).to have_css('a', text: 'first mentor')
+    expect(page).to have_css('a', text: 'second mentor')
+  end
 end
