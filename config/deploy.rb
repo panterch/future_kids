e5c42938e2bdffe0f05ee7075ed0f89cafc935b8
Kaspar Vollenweider <vok@panter.ch>
2020-05-25 19:03:36 +0200

    remove panter rails deploy in order to remove unicorn

--- config/deploy.rb
+++
@@ -1,5 +0,0 @@
-set :application, 'futurekids'
-set :repo_url, 'git@github.com:panterch/future_kids.git'
-
-set :linked_files, fetch(:linked_files, []).push('config/secrets.yml')
-set :linked_dirs, fetch(:linked_dirs, []).push('public/system')
