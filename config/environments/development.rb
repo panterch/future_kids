76c791d7af2d6a7cdf234711f5a181af08830e72
Jakub Motycka <jakubonty@centrum.cz>
2020-05-26 18:09:26 +0200

    Fix development storage config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,5 +63,5 @@ Rails.application.configure do
    # routes, locales, etc. This feature depends on the listen gem.
    config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-   config.active_storage.service = :amazon
+   config.active_storage.service = :local
 end
