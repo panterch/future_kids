ffdf52cb712acc4312ef2316251c98dfb22d1398
Beat Seeliger <seb@panter.ch>
2020-07-07 10:56:06 +0200

    heroku metrics

--- Gemfile
+++ Gemfile
@@ -35,7 +35,8 @@ gem 'rubyzip', '>= 1.2.1'
 gem 'caxlsx'
 gem 'caxlsx_rails'
 
-gem 'puma' # needed for travis, see https://github.com/panterch/future_kids/issues/223
+gem 'puma'
+gem 'barnes'
 
 # Bundle gems for the local environment. Make sure to
 # put test-only gems in this group so their generators
--- config/puma.rb
+++ config/puma.rb
@@ -1,3 +1,5 @@
+require 'barnes'
+
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers: a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
@@ -36,3 +38,7 @@ preload_app!
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+before_fork do
+  Barnes.start # Must have enabled worker mode for this to block to be called
+end
