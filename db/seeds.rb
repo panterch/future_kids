a80334730572b580a76b7701d652288924fa7cba
Ales Rosina <ales.rosina@gmail.com>
2021-07-02 16:13:27 +0200

    fix: seeds with working data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,32 +6,40 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
+schools = []
+unless School.count > 0
+  schools << School.create!(name: 'Primary school', school_kind: 'primary_school').id
+  schools << School.create!(name: 'Secondary school', school_kind: 'secondary_school').id
+  schools << School.create!(name: 'Gymnasium', school_kind: 'gymnasium').id
+  schools << School.create!(name: 'High school', school_kind: 'high_school').id
+end
+
 unless Mentor.count > 0
-  Mentor.create!(name: 'Haller', prename: 'Frederik',
+  Mentor.create!(name: 'Haller', prename: 'Frederik', state: 'accepted',
                  email: 'mentor@example.com', password: 'welcome',
-                 password_confirmation: 'welcome')
-  Mentor.create!(name: 'Rohner', prename: 'Melanie',
+                 password_confirmation: 'welcome', sex: 'm', school_id: schools[2])
+  Mentor.create!(name: 'Rohner', prename: 'Melanie', state: 'accepted',
                  email: 'mentor2@example.com', password: 'welcome',
-                 password_confirmation: 'welcome')
-  Mentor.create!(name: 'Steiner', prename: 'Max',
+                 password_confirmation: 'welcome', sex: 'f', school_id: schools[2])
+  Mentor.create!(name: 'Steiner', prename: 'Max', state: 'accepted',
                  email: 'mentor3@example.com', password: 'welcome',
-                 password_confirmation: 'welcome')
+                 password_confirmation: 'welcome', sex:'m', school_id: schools[3])
 end
 
 unless Admin.count > 0
-  Admin.create!(name: 'AOZ', prename: 'Admin',
+  Admin.create!(name: 'AOZ', prename: 'Admin', state: 'accepted',
                 email: 'admin@example.com', password: 'welcome',
                 password_confirmation: 'welcome')
 end
 
 unless Teacher.count > 0
-  Teacher.create!(name: 'Meckler', prename: 'Janine',
+  Teacher.create!(name: 'Meckler', prename: 'Janine', state: 'accepted',
                   email: 'teacher@example.com', password: 'welcome',
-                  password_confirmation: 'welcome')
+                  password_confirmation: 'welcome', sex: 'f', school_id: schools[0])
 end
 
 unless Kid.count > 0
-  Kid.create!(name: 'Meier', prename: 'Max')
-  Kid.create!(name: 'Hodler', prename: 'Rolf')
-  Kid.create!(name: 'Schwarz', prename: 'Sandra')
+  Kid.create!(name: 'Meier', prename: 'Max', sex: 'm')
+  Kid.create!(name: 'Hodler', prename: 'Rolf', sex: 'm')
+  Kid.create!(name: 'Schwarz', prename: 'Sandra', sex: 'f')
 end
