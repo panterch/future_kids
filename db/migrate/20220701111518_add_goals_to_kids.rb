b4c848939b45e68dd8ce9fe0713531bd36b4e98b
Beat Seeliger <seb@panter.ch>
2022-07-02 16:48:42 +0200

    splitted up goals into more facetted form

---
+++ db/migrate/20220701111518_add_goals_to_kids.rb
@@ -0,0 +1,37 @@
+class AddGoalsToKids < ActiveRecord::Migration[6.1]
+  def change
+    add_column :kids, :goal_3, :boolean
+    add_column :kids, :goal_4, :boolean
+    add_column :kids, :goal_5, :boolean
+    add_column :kids, :goal_6, :boolean
+    add_column :kids, :goal_7, :boolean
+    add_column :kids, :goal_8, :boolean
+    add_column :kids, :goal_9, :boolean
+    add_column :kids, :goal_10, :boolean
+    add_column :kids, :goal_11, :boolean
+    add_column :kids, :goal_12, :boolean
+    add_column :kids, :goal_13, :boolean
+    add_column :kids, :goal_14, :boolean
+    add_column :kids, :goal_15, :boolean
+    add_column :kids, :goal_16, :boolean
+    add_column :kids, :goal_17, :boolean
+    add_column :kids, :goal_18, :boolean
+    add_column :kids, :goal_19, :boolean
+    add_column :kids, :goal_20, :boolean
+    add_column :kids, :goal_21, :boolean
+    add_column :kids, :goal_22, :boolean
+    add_column :kids, :goal_23, :boolean
+    add_column :kids, :goal_24, :boolean
+    add_column :kids, :goal_25, :boolean
+    add_column :kids, :goal_26, :boolean
+    add_column :kids, :goal_27, :boolean
+    add_column :kids, :goal_28, :boolean
+    add_column :kids, :goal_29, :boolean
+    add_column :kids, :goal_30, :boolean
+    add_column :kids, :goal_31, :boolean
+    add_column :kids, :goal_32, :boolean
+    add_column :kids, :goal_33, :boolean
+    add_column :kids, :goal_34, :boolean
+    add_column :kids, :goal_35, :boolean
+  end
+end
